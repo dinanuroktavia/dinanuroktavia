80fd56056380249061574bede993ec2c19754144
dinanuroktavia <nuroktaviadina@gmail.com>
2018-01-29 07:14:20 +0800

    praktikum03

---
+++ NO1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <conio.h>
+#include <cmath>
+
+using namespace std;
+
+int main () {
+	int x, y, z;
+	cout << "Input nilai x = ";
+	cin  >> x;
+	
+	cout << "Input nilai y = ";
+	cin  >> y;
+	
+	if (x > y)	{
+	z = y - x;	
+		cout<<abs (z)<<endl;
+		cout<<"Hasil Positif"; 
+	
+}
+
+
+	    else if (z < 0) {
+		z = z * (-1) ;
+		cout<<abs (x-y)<<endl;
+		cout<<"Hasil Positif";
+	}
+	
+}
